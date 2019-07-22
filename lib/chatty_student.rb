72ba24bca3e871f8255974962a87af83fa8c837a
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-22 02:12:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,6 +9,7 @@ class ChattyStudent < Student
 
   def raise_hand
     # 10.times {puts super}
-    super super super super super super super super super super
+    super
+    super super super super super super super super super
   end
 end
